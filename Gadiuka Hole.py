61dcc31186b0c52c088131e782d0590ef14af8e8
a67715643-crypto <a67715643@gmail.com>
2026-04-19 18:21:35 +0300

    Add files via upload

---
+++ Gadiuka Hole.py
@@ -0,0 +1,301 @@
+import colorama
+
+colorama.init()
+
+print(colorama.Fore.BLACK)
+print(colorama.Back.GREEN)
+
+print('🐍Gadiuka Cafe🐍')
+
+print(colorama.Back.LIGHTWHITE_EX)
+
+print("""instruction:
+      print takeX2 to take;
+      print jumpX2 to jump;
+      print goX2 to go;
+      print openX2 to open;
+      print fightX2 to fight.""")
+
+print(colorama.Back.YELLOW)
+
+print('Gadiuka: Hi! I saw that the python is too scared today! What can it be?')
+
+print(colorama.Back.BLACK)
+print(colorama.Fore.LIGHTWHITE_EX)
+print("""
+       _________
+      /        /
+     /________/
+""")
+print('Gadiuka: What? Why the trapdoor is black?')
+print('Me: Wha- What H-how it became black?')
+print('Me: We need to go to the Underworld! We MUST know what it is!')
+
+print('trapdoor')
+print('go?')
+
+act = input()
+
+input()
+
+if input() == 'go':
+    print(colorama.Back.RED)
+    print(colorama.Fore.LIGHTYELLOW_EX)
+    print('Welcome to the Underworld')
+    print(colorama.Fore.BLACK)
+    print(colorama.Back.LIGHTBLACK_EX)
+
+    print('paper')
+    print('take?')
+
+    input()
+
+    if input() == 'take':
+        print(colorama.Back.LIGHTWHITE_EX)
+        print('Something appeared in the Underworld...')
+
+    print(colorama.Back.LIGHTBLACK_EX)
+
+    print('big mineshaft')
+    print('go?')
+
+    input()
+
+    if input() == 'go':
+        print(colorama.Back.YELLOW)
+        print("""--------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------""")
+
+        print(colorama.Back.LIGHTBLACK_EX)
+
+        print('paper')
+        print('take?')
+
+        input()
+
+        if input() == 'take':
+            print(colorama.Back.LIGHTWHITE_EX)
+            print('The world will never be the same...')
+
+            print(colorama.Back.LIGHTBLACK_EX)
+
+        print('Gadiuka\'s paper')
+        print('take?')
+
+        input()
+
+        if input() == 'take':
+            print(colorama.Back.LIGHTWHITE_EX)
+            print('What are these papers about? What is \'something\'?')
+
+        print(colorama.Back.LIGHTBLACK_EX)
+
+        print('cave')
+        print('jump?')
+
+        input()
+
+        if input() == 'jump':
+            print('------____------')
+print(colorama.Back.BLACK)
+print(colorama.Fore.LIGHTWHITE_EX)
+
+print('-----------____-----')
+
+print(colorama.Back.LIGHTBLACK_EX)
+print(colorama.Fore.BLACK)
+
+print('--------____------')
+
+print('mineshaft')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print(colorama.Back.YELLOW)
+    print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+    print(colorama.Back.LIGHTBLACK_EX)
+
+    print('cave')
+    print('jump?')
+
+    input()
+
+    if input() == 'jump':
+        print("""--____----------
+----------____---""")
+print(colorama.Back.BLACK)
+print(colorama.Fore.LIGHTWHITE_EX)
+
+print('----------____---')
+
+print(colorama.Back.LIGHTBLACK_EX)
+print(colorama.Fore.BLACK)
+
+print('Me: What happened here? Why some parts of the caves are black? But... it happened to the trapdoor too... I can not understand this.')
+
+print('lower cave')
+print('go?')
+
+input()
+
+if input() == 'go':
+    print("""                              \    \ 
+                               \    \ 
+                                \    \ 
+                                 \    \ 
+                                  \    \ 
+                                   \    \ 
+                                    \    \ 
+                                     \    \ ________
+                                      \     
+                                       \____________""")
+
+    print('Gadiuka\'s paper')
+    print('take?')
+
+    input()
+
+    if input() == 'take':
+        print(colorama.Back.LIGHTWHITE_EX)
+        print('What is this black color? I never saw it before!')
+
+        print(colorama.Back.LIGHTBLACK_EX)
+
+        print('mineshaft')
+        print('go?')
+
+        input()
+
+        if input() == 'go':
+            print(colorama.Back.YELLOW)
+            print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+            print(colorama.Back.LIGHTBLACK_EX)
+
+            print('paper')
+            print('take?')
+
+            input()
+
+            if input() == 'take':
+                print(colorama.Back.LIGHTWHITE_EX)
+                print('Some▮hing is growing...')
+
+            print(colorama.Back.LIGHTBLACK_EX)
+
+            print('Gadiuka\'s paper')
+            print('take?')
+
+            input()
+
+            if input() == 'take':
+                print(colorama.Back.LIGHTWHITE_EX)
+                print('WHAT!? ARE THESE THE NEW SYMBO▮S OR WHAT!? The bl▮ck thing came even to the text! P▮epare yourself!')
+
+            print(colorama.Back.LIGHTBLACK_EX)
+
+            print('mineshaft')
+            print('go?')
+
+            input()
+
+            if input() == 'go':
+                print(colorama.Fore.LIGHTWHITE_EX)
+                print(colorama.Back.BLACK)
+                print("""----------------------------------------------------------------
+----------------------------------------------------------------""")
+
+            print(colorama.Fore.BLACK)
+            print(colorama.Back.LIGHTBLACK_EX)
+
+            print('cave')
+            print('jump?')
+
+            input()
+
+            if input() == 'jump':
+                print('----____-------')
+                print(colorama.Back.BLACK)
+                print(colorama.Fore.LIGHTWHITE_EX)
+                print("""-------_____---
+-------_____-----""")
+
+                print(colorama.Back.LIGHTBLACK_EX)
+                print(colorama.Fore.BLACK)
+
+                print('big mineshaft')
+                print('go?')
+
+                input()
+
+                if input() == 'go':
+                    print(colorama.Back.YELLOW)
+                    print("""--------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------""")
+
+                print(colorama.Back.LIGHTBLACK_EX)
+
+                print('cave')
+                print('jump?')
+
+                input()
+
+                if input() == 'jump':
+                    print(colorama.Back.BLACK)
+                    print(colorama.Fore.LIGHTWHITE_EX)
+                    print('----______-----')
+                    print(colorama.Back.LIGHTBLACK_EX)
+                    print(colorama.Fore.BLACK)
+                    print("""---____-----
+--_____-------""")
+
+                print('Gadiuka\'s paper')
+                print('take?')
+
+                input()
+
+                if input() == 'take':
+                    print(colorama.Back.LIGHTWHITE_EX)
+                    print(
+                        'I... I fou▮d some▮▮ing... I touche▮ the black ▮hing and... it... it was co▮d and a▮▮olutely emp▮y...')
+
+                    print(colorama.Back.LIGHTBLACK_EX)
+
+                    print('paper')
+                    print('take?')
+
+                    input()
+
+                    if input() == 'take':
+                        print(colorama.Back.LIGHTWHITE_EX)
+                        print('It is ge▮ting bigger ev▮ry time ▮t eats...')
+
+                        print(colorama.Back.LIGHTBLACK_EX)
+
+                    print('big cave')
+                    print('jump?')
+
+                    input()
+
+                    if input() == 'jump':
+                        print("""-----____----
+----_____-----
+---____-------
+---------_____---
+----____------""")
+                    print(colorama.Back.BLACK)
+                    print(colorama.Fore.LIGHTWHITE_EX)
+                    print("""-----____---
+---_____------
+----____-------
+----------___---
+-------______---
+--______-----
+-----____----""")
+
+print('You were hurt...')
